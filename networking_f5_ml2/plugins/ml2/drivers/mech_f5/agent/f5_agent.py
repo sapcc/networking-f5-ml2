c1d15301e7b38310ed4dc515c384e4eb86bbb566
Andrew Battye <andrew.battye@sap.com>
2016-03-17 08:54:43 +0000

    Add F5 updates

--- networking_f5_ml2/plugins/ml2/drivers/mech_f5/agent/f5_agent.py
+++ networking_f5_ml2/plugins/ml2/drivers/mech_f5/agent/f5_agent.py
@@ -289,7 +289,7 @@ class F5NeutronAgent(sg_rpc.SecurityGroupAgentRpcCallbackMixin):
 
         all_ports = self.db.get_ports(self.context_with_session,filters={'host' : self.agent_host})
 
-        for bigip in self.f5_driver.bigips:
+        for bigip in self.f5_driver.get_config_bigips():
             vlans = bigip.vlan.get_vlans(folder='/')
             LOG.info("******** VLANs from F5")
             LOG.info("******** vlans")
